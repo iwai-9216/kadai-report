f4133dbcd62344e49a8c7106acc5cd0907932ae5
kunihiro.iwai <iwai9216soft@gmail.com>
2020-05-30 15:39:22 +0900

    Add Employee new

--- src/controllers/employees/EmployeesNewServlet.java
+++ src/controllers/employees/EmployeesNewServlet.java
@@ -2,11 +2,14 @@ package controllers.employees;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
 
 @WebServlet("/employees/new")
 public class EmployeesNewServlet extends HttpServlet {
@@ -17,7 +20,10 @@ public class EmployeesNewServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("employee", new Employee());
 
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+        rd.forward(request, response);
     }
 }
